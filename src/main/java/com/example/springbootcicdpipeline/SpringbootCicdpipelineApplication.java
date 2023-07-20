6df3ea56f5897f9310dd4a8a7152fafae40e9c0f
NIKHIL CC <cc.nikhil@gmail.com>
2023-07-20 17:58:48 +0530

    first commit

---
+++ src/main/java/com/example/springbootcicdpipeline/SpringbootCicdpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdpipelineApplication.class, args);
+	}
+
+}
